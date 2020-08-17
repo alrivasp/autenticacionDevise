7e3df8253786e738b81303d87252e70df10011aa
alrivasp <alvaro.rivas.p@gmail.com>
2020-08-16 23:28:21 -0400

    Update Desafio 2 full

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_pirmitted_parameters, if: :devise_controller?
+
+  protected
+  def configure_pirmitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+  
+  def authenticate_admin!
+    unless current_user.present? && current_user.is_admin?
+      redirect_to root_path, notice: "No tiene permisos esta seccion"
+    end
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'admin/index'
+  get 'users/edit/:id', to: 'admin#edit', as: 'edit_user'
+  patch 'users/update/:id', to: 'admin#update', as: 'update_user'
+  get 'my_stories',to: 'stories#my_stories'
+  devise_for :users
   resources :stories
   root 'stories#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
